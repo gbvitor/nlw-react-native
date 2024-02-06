2ca235a5d40fdaa8b7e9a57769fa2df27270f1cb
Vitor Gabriel <sgvitor@outlook.com.br>
2024-02-06 02:35:09 -0300

    instala tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+//npx tailwindcss init
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/app/**/*.{ts,tsx}", "./src/components/**/*.{ts,tsx}"],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
