fb058c5935f709b6d1e485d23e4c26988b148904
Vitor Gabriel <sgvitor@outlook.com.br>
2024-02-06 02:36:25 -0300

    cria estrutura básica

---
+++ src/app/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function Home() {
+    return (
+        <View className="bg-slate-900">
+            <Text>Hello! React-Native</Text>
+        </View>
+    );
+}
