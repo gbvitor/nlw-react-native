70ca8e5f7e9e5dd3b5459d32709085084d38175d
Vitor Gabriel <sgvitor@outlook.com.br>
2024-02-06 18:13:10 -0300

    cria pasta components

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,7 +1,24 @@
 import { Slot } from "expo-router";
 import { SafeAreaView } from "react-native";
+import {
+    useFonts,
+    Inter_400Regular,
+    Inter_500Medium,
+    Inter_600SemiBold,
+    Inter_700Bold,
+} from "@expo-google-fonts/inter";
+import { Loading } from "@/components/loading";
 
 export default function _layout() {
+    const [fontLoader] = useFonts({
+        Inter_400Regular,
+        Inter_500Medium,
+        Inter_600SemiBold,
+        Inter_700Bold,
+    });
+    if (!fontLoader) {
+        return <Loading />;
+    }
     return (
         <SafeAreaView className="bg-slate-900 flex-1">
             <Slot />
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,7 +2,7 @@ import { View, Text } from "react-native";
 
 export default function Home() {
     return (
-        <View>
+        <View className="flex-1 pt-8">
             <Text className="text-white">Hello! React-Native</Text>
         </View>
     );
